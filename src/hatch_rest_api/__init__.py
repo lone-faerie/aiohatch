f36aa251bb9776f592280a4b7eb9c0f615b7679b
lone-faerie <87098960+lone-faerie@users.noreply.github.com>
2023-03-30 19:52:24 -0700

    Update __init__.py

--- src/hatch_rest_api/__init__.py
+++ src/hatch_rest_api/__init__.py
@@ -5,10 +5,13 @@ from .rest_mini import RestMini
 from .rest_plus import RestPlus
 from .riot import RestIot
 from .restoreiot import RestoreIot
+from .restore import Restore
 from .util_bootstrap import get_rest_devices
 from .const import (
     RestMiniAudioTrack,
     REST_MINI_AUDIO_TRACKS,
     RestPlusAudioTrack,
     REST_PLUS_AUDIO_TRACKS,
+    RestoreColor,
+    RESTORE_COLORS,
 )
